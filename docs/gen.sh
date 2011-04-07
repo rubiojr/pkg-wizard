8acb85ef7184a0d138029cbdbd7d63431f686794
Sergio Rubio <sergio@rubio.name>
2011-04-07 13:00:14 +0200

    * Added doc generator script

---
+++ docs/gen.sh
@@ -0,0 +1,6 @@
+ronn --organization="The FrameOS PROJECT" --manual=INSTALLATION install.md
+ronn --organization="The FrameOS PROJECT" --manual="remote-build" remote-build.md
+ronn --organization="The FrameOS PROJECT" --manual="build-bot" build-bot.md
+ronn --organization="The FrameOS PROJECT" --manual="pkgwiz" pkgwiz.md
+ronn --organization="The FrameOS PROJECT" --manual="create-srpm" create-srpm.md
+ronn --organization="The FrameOS PROJECT" --manual="init-env" init-env.md
